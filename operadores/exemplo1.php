2a36048c82e58087f9da78e6aa5bedbd51096eda
Airton Dambrovski <tooonjr@gmail.com>
2020-01-08 13:18:04 -0300

    aulas com operadores php

---
+++ operadores/exemplo1.php
@@ -0,0 +1,13 @@
+<?php
+
+$nome = "Airton";
+
+echo $nome . " Silva";
+
+echo "<br />";
+
+$nome .= " Dambrovski";
+
+echo $nome;
+
+?>
---
+++ operadores/exemplo2.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$valorTotal = 0;
+$valorTotal += 100;
+$valorTotal += 25;
+
+echo $valorTotal;
+
+echo "<br />";
+$valorTotal -= 10;
+
+echo $valorTotal;
+
+?>
