aa1a0c83bf3b796bac58854004173db50c3d7c6d
Airton Silva <tooonjr@gmail.com>
2020-02-13 22:28:09 -0300

    finalização dos exemplos usando DAO e CRUD simples

--- DAO/index.php
+++ DAO/index.php
@@ -8,11 +8,44 @@ $sql = new Sql();
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
 */
+//carrega um usuario
+//$root = new Usuario();
+//$root->loadbyId(2);
+//echo $root;
 
-$root = new Usuario();
+//carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-$root->loadbyId(3);
+//carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("j");
+//echo json_encode($search);
 
-echo $root;
+
+//carrega um usuario usando o login e senha
+//$usuario = new Usuario();
+//$usuario->login("jose","1234567");
+//echo $usuario;
+
+//CADASTRAR ALUNO NOVO
+//$aluno = new Usuario("julio","senha");
+//$aluno->insert();
+//echo $aluno;
+
+//UPDATE DE UM ALUNO
+//$aluno = new Usuario();
+//$aluno->loadById(12);
+//echo $aluno;
+//echo "<br />";
+//$aluno->update("professor", "kkksouprof");
+//echo $aluno;
+
+//DELETE DE UM ALUNO
+$aluno = new Usuario();
+$aluno->loadbyId(13);
+echo $aluno;
+echo "<br />";
+$aluno->delete();
+echo $aluno;
 
 ?>
