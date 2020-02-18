7b7b4f0ba5e9a35e57729087a42f9633df37e2d9
Airton Silva <tooonjr@gmail.com>
2020-02-18 00:56:21 -0300

    mais exercicios em php usando CURL

---
+++ rename/exemplo1.php
@@ -0,0 +1,24 @@
+<?php
+
+$dir1 = "folder_01";
+$dir2 = "folder_02";
+
+if(!is_dir($dir1)) mkdir($dir1);
+if(!is_dir($dir2)) mkdir($dir2);
+
+$filename = "READER.txt";
+if (!file_exists($dir1 . DIRECTORY_SEPARATOR .$filename)){
+
+    $file = fopen($dir1 . DIRECTORY_SEPARATOR .$filename, "w+");
+    fwrite($file, date("Y-m-d H:i:S"));
+    fclose($file);
+}
+
+rename(
+    $dir1 . DIRECTORY_SEPARATOR .$filename, //ORIGEM
+    $dir2 . DIRECTORY_SEPARATOR .$filename //DESTINO
+);
+
+echo "Arquivo movido com sucesso";
+
+?>
